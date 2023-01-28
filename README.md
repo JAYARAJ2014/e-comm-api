d7ed6cb201a1dc94db18d450afd09c5419053f12
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-01-28 11:18:01 -0500

    Setup Database connection

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # e-comm-api
 A sample e-comm project in NodeJS, TypeScript, MongoDb that makes use of JWT, Cookies, Stripe, Image Upload and Emailing
+
+
+
+### Steps
+
+
+Create an express server app. -- app.ts
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
-import dotenv from 'dotenv'
-import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-
+import { connectDb } from './db/connect';
 
 dotenv.config();
 
-const PORT = process.env.PORT ||  3000;
-const app:Express = express();  
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || '';
 
-app.use(express.json())
-app.use('/',(req : Request, res:Response)=> {
-    return res.status(StatusCodes.OK).json( {message: 'e-commerce API'})
-})
+const app: Express = express();
 
-const appStart = async ()=> {
-    try {
-        app.listen(PORT, ()=>{
-            console.log(`Express Server listening at PORT: ${PORT}`)
-        })    
-    } catch (error) {
-        console.log(`Exceptoin Occured at Startup : ${error}`)
-    }
-    
-}
+app.use(express.json());
+app.use('/', (req: Request, res: Response) => {
+  return res.status(StatusCodes.OK).json({ message: 'e-commerce API' });
+});
+
+const appStart = async () => {
+  try {
+    await connectDb(MONGO_URI);
+    console.log(`Established database connection`)
+    app.listen(PORT, () => {
+      console.log(`Express Server listening at PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Exceptoin Occured at Startup : ${error}`);
+  }
+};
 
 appStart();
