83f2297df80572c6c9c3dcb4f30d288ceaf9480c
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-01-28 18:17:51 -0500

    Added basic logic to check if email exists and save User. Set first user as admin

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -1,58 +1,29 @@
 import { Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { User } from '../models/user';
-import { StatusCodes } from 'http-status-codes';
+import { IUser, User } from '../models/user';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { BadRequestError, UnAuthorizedError } from '../custom-errors/';
 import bcrypt from 'bcryptjs';
+import { rmSync } from 'fs';
 
 class AuthHandler {
   public async register(req: Request, res: Response) {
-    const { username, email, password } = req.body;
-    if (!username || !password || !email) {
-      throw new BadRequestError('Required input is not provided.');
+    const {email,name , password ,role :UserRoleEnum} = req.body;
+    const alredyExistingUser :IUser | null =await User.findOne({email: email});
+    
+    if(alredyExistingUser) {
+        throw new BadRequestError('The specified email  already exists')
     }
+    const firstUser: boolean = await User.countDocuments({})===0;
+    const role = firstUser ? 'ADMIN' :'USER'
+    const user = await User.create({name, email, password,role})
+    res.status(StatusCodes.CREATED).json(user)
 
-    const user = await User.create({ ...req.body });
-    const token = jwt.sign(
-      { userId: user._id, name: user.name },
-      process.env.JWT_SECRET as string,
-      {
-        expiresIn: process.env.JWT_LIFE_TIME as string
-      }
-    );
-    console.log(`User Created:  `, user);
-    return res
-      .status(StatusCodes.OK)
-      .json({ user: { userId: user._id, name: username }, token: token });
   }
   public async login(req: Request, res: Response) {
-    const { email, password } = req.body;
-    if (!password || !email) {
-      throw new BadRequestError('email / Password must be provided.');
-    }
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw new UnAuthorizedError('User does not exist');
-    }
-
-    // Compared the passwords
-
-    const isPasswordsMatching = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordsMatching) {
-      throw new UnAuthorizedError('Invalid Password');
-    }
-
-    const token = jwt.sign(
-      { userId: user._id, name: user.name },
-      process.env.JWT_SECRET as string,
-      {
-        expiresIn: process.env.JWT_LIFE_TIME as string
-      }
-    );
-
-    return res.status(StatusCodes.OK).json({ name: user.name, token });
+    
+    const {email, password}=req.body;
+    return res.status(StatusCodes.OK).json({ name: 'hELLO' });
   }
 
   public async logout(req: Request, res: Response) {
