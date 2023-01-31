d1257f478a21f829a9ced9dd94e933bb4b949ded
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-01-30 19:43:51 -0500

    Added logic for Create Product ,Get Product(s) and Delete Product

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { IUser, User } from '../models/user';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
-import { BadRequestError, UnAuthorizedError } from '../custom-errors/';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthorizedError
+} from '../custom-errors/';
 import bcrypt from 'bcryptjs';
 import { rmSync } from 'fs';
 import { JwtUtil } from '../utils/jwt';
@@ -10,24 +14,37 @@ import { IProduct, Product } from '../models/product';
 
 class ProductHandler {
   public async createProduct(req: Request, res: Response) {
-      req.body.user= req.user?.userId
-      const product: IProduct = await Product.create(req.body);
-    res.status(StatusCodes.CREATED).json({product})
+    req.body.user = req.user?.userId;
+    const product: IProduct = await Product.create(req.body);
+    res.status(StatusCodes.CREATED).json({ product });
   }
 
   public async getAllProducts(req: Request, res: Response) {
-    res.send('getAllProducts');
+    const products: IProduct[] = await Product.find({}).select('-user');
+    res.status(StatusCodes.OK).json(products);
   }
 
   public async getSingleProduct(req: Request, res: Response) {
-    res.send('getSingleProduct');
+    const product = await Product.findOne({ _id: req.params.id });
+    if (!product) {
+      throw new NotFoundError(
+        `Product with Id ${req.params.id} does not exist`
+      );
+    }
+    res.status(StatusCodes.OK).json(product);
   }
 
-  public async updateProduct(req: Request, res: Response) {
-    res.send('updateProduct');
-  }
+  public async updateProduct(req: Request, res: Response) {}
   public async deleteProduct(req: Request, res: Response) {
-    res.send('deleteProduct');
+    const product = await Product.findOneAndDelete({ _id: req.params.id });
+    if (!product) {
+      throw new NotFoundError(
+        `Product with Id ${req.params.id} does not exist`
+      );
+    }
+    res
+      .status(StatusCodes.OK)
+      .json({ message: `Product Deleted`, product: product });
   }
 
   public async uploadImage(req: Request, res: Response) {
