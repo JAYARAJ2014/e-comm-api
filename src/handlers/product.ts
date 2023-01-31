172444b1c51afe57461bd09c52eacccfc3a9cadd
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-01-30 21:03:32 -0500

    Added count of products in the products list APi

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -21,11 +21,13 @@ class ProductHandler {
 
   public async getAllProducts(req: Request, res: Response) {
     const products: IProduct[] = await Product.find({}).select('-user');
-    res.status(StatusCodes.OK).json(products);
+      res.status(StatusCodes.OK).json({ products: products, count: products.length } );
   }
 
   public async getSingleProduct(req: Request, res: Response) {
-    const product = await Product.findOne({ _id: req.params.id });
+    const product = await Product.findOne({ _id: req.params.id }).select(
+      '-user'
+    );
     if (!product) {
       throw new NotFoundError(
         `Product with Id ${req.params.id} does not exist`
@@ -34,9 +36,26 @@ class ProductHandler {
     res.status(StatusCodes.OK).json(product);
   }
 
-  public async updateProduct(req: Request, res: Response) {}
+  public async updateProduct(req: Request, res: Response) {
+    const product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+    if (!product) {
+      throw new NotFoundError(
+        `Product with Id ${req.params.id} does not exist`
+      );
+    }
+    res
+      .status(StatusCodes.OK)
+      .json({ message: `Product Updated`, product: product });
+  }
   public async deleteProduct(req: Request, res: Response) {
-    const product = await Product.findOneAndDelete({ _id: req.params.id });
+    const product = await Product.findOne({ _id: req.params.id });
     if (!product) {
       throw new NotFoundError(
         `Product with Id ${req.params.id} does not exist`
