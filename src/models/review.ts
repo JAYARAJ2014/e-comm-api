fcfcfc12acfe474b448671ab8a6eeddcfc1b5279
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-02-02 18:59:07 -0500

    Created Review Schema

--- src/models/review.ts
+++ src/models/review.ts
@@ -9,39 +9,40 @@ export interface IReview {
   user: mongoose.Schema.Types.ObjectId;
 }
 
-const ReviewSchema = new Schema<IReview>({
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-    required:[true, 'A Rating must be provided']
+const ReviewSchema = new Schema<IReview>(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'A Rating must be provided']
     },
     title: {
-        type: String,
-        minlength: 5,
-        max: 100,
-        trim:true,
-        required:[true, 'A Title must be provided']
+      type: String,
+      minlength: 5,
+      max: 100,
+      trim: true,
+      required: [true, 'A Title must be provided']
     },
     comment: {
-        type: String,
-        minlength: 3,
-        max: 1000,
-        required:[true, 'A Comment must be provided']
+      type: String,
+      minlength: 3,
+      max: 1000,
+      required: [true, 'A Comment must be provided']
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-
-        
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
     },
     product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required:true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
     }
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
-ReviewSchema.index({product:1, user: 1}, {unique: true})
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 export const Review = model<IReview>('Review', ReviewSchema);
