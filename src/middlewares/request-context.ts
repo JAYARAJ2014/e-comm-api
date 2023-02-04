a4f8763794de0e45dd2626e3bd321f9b44ddf3fe
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-02-03 21:55:42 -0500

    prettified

--- src/middlewares/request-context.ts
+++ src/middlewares/request-context.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidV4 } from 'uuid';
-export const requestContext = (req: Request, res: Response, next: NextFunction) => {
-    const requestId= req.headers['ecomm-api-request-id'] || 'auto-'+uuidV4()
-    
-        
-        res.setHeader('ecomm-api-request-id',requestId );
-    
-    next();
+export const requestContext = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const requestId = req.headers['ecomm-api-request-id'] || 'auto-' + uuidV4();
+
+  res.setHeader('ecomm-api-request-id', requestId);
+
+  next();
 };
